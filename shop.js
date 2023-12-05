04d20c654d6ccaf1de93ed23f9a64d8c7dc3a70c
teucriumveronica <150184524+teucriumveronica@users.noreply.github.com>
2023-12-05 20:46:59 +0300

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,27 @@
+async function getResponce() {
+//    let responce = await fetch("shop.json")
+
+//    let content = await responce.text()
+    let response = await fetch("shop.json");
+    let content = await responce.json();
+//    content = content.slice(0, 9)
+    let key
+//    for (key in content) {
+//        console.log(content[key].id, content[key].title)
+//        console.log(content[key])
+//    }
+
+    let node_for_insert = document.getElementById("node_for_insert")
+    for (key in content) {
+    let li = document.createElement("li");
+    li.style.width = "210px";
+    li.className = "d-flex flex-column m-1 p-1 border bg-body";
+
+    // Другие элементы и их стили
+
+    node_for_insert.appendChild(li);
+}
+
+}
+getResponce()
+
